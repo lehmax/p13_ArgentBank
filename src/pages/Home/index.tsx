57e659afce771179b0a56b080187874e25f05cf4
Maxime Leherquer <45520008+lehmax@users.noreply.github.com>
2024-06-30 22:13:29 +0200

    refacto

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,34 +1,60 @@
-import { Hero } from '../../components/Hero'
-
 import iconChat from '../../assets/icon-chat.png'
 import iconMoney from '../../assets/icon-money.png'
 import iconSecurity from '../../assets/icon-security.png'
 
-interface Feature {
-  icon: string
-  title: string
-  text: string
+const Home = () => {
+  return (
+    <main>
+      <Hero />
+      <Features />
+    </main>
+  )
 }
 
-const features: Feature[] = [
-  {
-    icon: iconChat,
-    title: 'You are our #1 priority',
-    text: 'Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.',
-  },
-  {
-    icon: iconMoney,
-    title: 'More savings means higher rates',
-    text: 'The more you save with us, the higher your interest rate will be!',
-  },
-  {
-    icon: iconSecurity,
-    title: 'Security you can trust',
-    text: 'We use top of the line encryption to make sure your data and money is always safe.',
-  },
-]
+const Hero = () => {
+  return (
+    <div className="hero">
+      <section className="hero-content">
+        <h2 className="sr-only">Promoted Content</h2>
+        <p className="subtitle">No fees.</p>
+        <p className="subtitle">No minimum deposit.</p>
+        <p className="subtitle">High interest rates.</p>
+        <p className="text">Open a savings account with Argent Bank today!</p>
+      </section>
+    </div>
+  )
+}
 
-const FeatureItem = ({ icon, title, text }: Feature) => {
+const Features = () => {
+  const features: Feature[] = [
+    {
+      icon: iconChat,
+      title: 'You are our #1 priority',
+      text: 'Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.',
+    },
+    {
+      icon: iconMoney,
+      title: 'More savings means higher rates',
+      text: 'The more you save with us, the higher your interest rate will be!',
+    },
+    {
+      icon: iconSecurity,
+      title: 'Security you can trust',
+      text: 'We use top of the line encryption to make sure your data and money is always safe.',
+    },
+  ]
+
+  return (
+    <section className="features">
+      <h2 className="sr-only">Features</h2>
+      {features.map((feature, index) => (
+        <FeatureItem key={index} {...feature} />
+      ))}
+    </section>
+  )
+}
+
+const FeatureItem = ({ icon, title, text }: FeatureItemProps) => {
   return (
     <div className="feature-item">
       <img src={icon} alt="" className="feature-icon" />
@@ -38,18 +64,12 @@ const FeatureItem = ({ icon, title, text }: Feature) => {
   )
 }
 
-const Home = () => {
-  return (
-    <main>
-      <Hero />
-      <section className="features">
-        <h2 className="sr-only">Features</h2>
-        {features.map((feature, index) => (
-          <FeatureItem key={index} {...feature} />
-        ))}
-      </section>
-    </main>
-  )
+interface Feature {
+  icon: string
+  title: string
+  text: string
 }
+
+interface FeatureItemProps extends Feature {}
 
 export default Home
--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -1,47 +1,75 @@
 const User = () => {
   return (
     <main className="main bg-dark">
-      <div className="header">
-        <h1>
-          Welcome back
-          <br />
-          Tony Jarvis!
-        </h1>
-        <button className="edit-button">Edit Name</button>
-      </div>
-      <h2 className="sr-only">Accounts</h2>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-          <p className="account-amount">$2,082.79</p>
-          <p className="account-amount-description">Available Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <button className="transaction-button">View transactions</button>
-        </div>
-      </section>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-          <p className="account-amount">$10,928.42</p>
-          <p className="account-amount-description">Available Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <button className="transaction-button">View transactions</button>
-        </div>
-      </section>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-          <p className="account-amount">$184.30</p>
-          <p className="account-amount-description">Current Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <button className="transaction-button">View transactions</button>
-        </div>
-      </section>
+      <Name />
+      <Accounts />
     </main>
   )
 }
+
+const Name = () => {
+  return (
+    <div className="header">
+      <h1>
+        Welcome back
+        <br />
+        Tony Jarvis!
+      </h1>
+      <button className="edit-button">Edit Name</button>
+    </div>
+  )
+}
+
+const Accounts = () => {
+  const accounts: Account[] = [
+    {
+      title: 'Argent Bank Checking (x8349)',
+      amount: '$2,082.79',
+      description: 'Available Balance',
+    },
+    {
+      title: 'Argent Bank Savings (x6712)',
+      amount: '$10,928.42',
+      description: 'Available Balance',
+    },
+    {
+      title: 'Argent Bank Credit Card (x8349)',
+      amount: '$184.30',
+      description: 'Current Balance',
+    },
+  ]
+
+  return (
+    <>
+      <h2 className="sr-only">Accounts</h2>
+      {accounts.map((account, index) => (
+        <Account key={index} {...account} />
+      ))}
+    </>
+  )
+}
+
+const Account = ({ title, amount, description }: AccountProps) => {
+  return (
+    <section className="account">
+      <div className="account-content-wrapper">
+        <h3 className="account-title">{title}</h3>
+        <p className="account-amount">{amount}</p>
+        <p className="account-amount-description">{description}</p>
+      </div>
+      <div className="account-content-wrapper cta">
+        <button className="transaction-button">View transactions</button>
+      </div>
+    </section>
+  )
+}
+
+interface Account {
+  title: string
+  amount: string
+  description: string
+}
+
+interface AccountProps extends Account {}
 
 export default User
